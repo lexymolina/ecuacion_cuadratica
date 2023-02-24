c977f9ccc62c041b66a68b716ffd78b7fd18d64a
sistemas <you@example.com>
2023-02-24 10:15:15 -0500

    se creo el diagrama de flujo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ecuacion_cuadratica
 programa para resolver una ecuación de segundo grado
+# diseño
+## diagrama de flujo
+![diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ ecuacion_cuadratica.py
@@ -0,0 +1,13 @@
+# programa para resolver una ecuacion de segundo grado
+
+# input
+a = int(input("digite el valor de a: "))
+b = int(input("digite el valor de b: "))
+c = int(input("digite el valor de c: "))
+
+#processing
+if ((b**2)-4*a*c) < 0:
+    print ("la solucion de la ecuacion es: ")
+    X1 = (-b + SQR(d)) / 2a
+    X2 = (-b - SQR(d)) / 2a
+    
