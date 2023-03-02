e4feeb04e277308330507ac19463db1afea206ff
sistemas <you@example.com>
2023-03-02 17:43:09 -0500

    se modifico ecuacion_cuadratica.py

--- ecuacion_cuadratica.py
+++ ecuacion_cuadratica.py
@@ -1,13 +1,29 @@
 # programa para resolver una ecuacion de segundo grado
 
-# input
-a = int(input("digite el valor de a: "))
-b = int(input("digite el valor de b: "))
-c = int(input("digite el valor de c: "))
+import math
+
+
+print("----------------------------------------------")
+print("------ Resolver la ecuación cuadrática -------")
+print("----------------------------------------------")
+
+#input
+
+a = int(input("Digite el valor de a: "))
+b = int(input("Digite el valor de b: "))
+c = int(input("Digite el valor de c: "))
 
 #processing
-if ((b**2)-4*a*c) < 0:
-    print ("la solucion de la ecuacion es: ")
-    X1 = (-b + SQR(d)) / 2a
-    X2 = (-b - SQR(d)) / 2a
+
+d = b**2-4*a*c
+if d == 0:
+    x1 = (d/2*a)
+    x2 = x1
+    print(x1, x2)
+if d > 0:
+    x1 =(-b+math.sqrt(d))/(2*a)
+    x2 =(-b-math.sqrt(d))/(2*a)
+    print(x1, x2)
+else:
+    print("la solución es imaginaria")
     
